84cbcbd642aa2724a317ff5b7f366a30872be6c3
Michael Hodges <michaelcshodges@gmail.com>
2019-11-28 16:33:23 -0800

    finalize styles for homepage title

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,13 +5,16 @@ import { PageFlexContainer } from './styles/styled_components/Containers'
 const Home = () => {
     return (
         <PageFlexContainer>
-            <div>
-                <h1>Functional</h1>
-                <h1> | </h1>
-                <h1>Innovative</h1>
+            <div className="title">
+                <h1 className="title--text">Functional</h1>
+                <h1 className="title--text__center"> | </h1>
+                <h1 className="title--text">Innovative</h1>
             </div>
-            <p>Michael Hodges</p>
-            <p>Fullstack Web Developer</p>
+            <div>
+                <p>Michael Hodges</p>
+                <hr/>
+                <p>Fullstack Web Developer</p>
+            </div>
             <a href="#aboutPage">
                 <img src='/icons/caret-down-solid.svg' alt='To About Page'></img>
             </a>
