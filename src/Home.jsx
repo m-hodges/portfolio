5d605c673d044d6bbcd289812f02e4e71e6cd4fc
Michael Hodges <michaelcshodges@gmail.com>
2019-11-29 01:22:57 -0800

    add mouse over event

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,38 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 import { PageFlexContainer } from './styles/styled_components/Containers'
 
-const Home = () => {
-    const state = {
-        isDisplayed: false
+class Home extends Component {
+    state = {
+        isDisplayed: true
+    }
+    
+    evt = () => {
+        (this.state.isDisplayed) 
+        ? this.setState({ isDisplayed: false })
+        : this.setState({ isDisplayed: true })
     }
 
-    return (
-        <PageFlexContainer>
-            <div className='spacer'></div>
-            <div className="title">
-                <h1 className="title--text">Functional</h1>
-                <h1 className="title--text__center"> | </h1>
-                <h1 className="title--text">Innovative</h1>
-            </div>
-            <div className='subtitle'>
-                <p className='subtitle--text subtitle--text__main'>
-                    Michael Hodges
-                    <hr className='subtitle--separator' />
-                </p>
-                <p className='subtitle--text subtitle--text__sub'>Fullstack Web Developer</p>
-            </div>
-            <div className='spacer spacer--container'>
-                <a href="#aboutPage" className='continue'>
-                    <ExpandMoreIcon fontSize='large'/>
-                </a>
-                {/* {this.state.isDisplayed && <p className='scroll'>Scroll</p>} */}
-            </div>
-            {/* bug: makes page load in about page - componentdidmount reset page position? */}
-        </PageFlexContainer>
-    );
+    render () {
+        return (
+            <PageFlexContainer>
+                <div className='spacer'></div>
+                <div className="title">
+                    <h1 className="title--text">Functional</h1>
+                    <h1 className="title--text__center"> | </h1>
+                    <h1 className="title--text">Innovative</h1>
+                </div>
+                <div className='subtitle'>
+                    <p className='subtitle--text subtitle--text__main'>
+                        Michael Hodges
+                        <hr className='subtitle--separator' />
+                    </p>
+                    <p className='subtitle--text subtitle--text__sub'>Fullstack Web Developer</p>
+                </div>
+                <div className='spacer spacer--container'>
+                    <a href="#aboutPage" className='continue' onMouseOver={this.evt} onMouseOut={this.evt}>
+                        {this.state.isDisplayed && <ExpandMoreIcon fontSize='large'/>}
+                        {!this.state.isDisplayed && <p className='scroll'>About</p>}
+                    </a>
+                </div>
+                {/* bug: makes page load in about page - componentdidmount reset page position? */}
+                {/* bug: onMouseOut event not working as intended */}
+            </PageFlexContainer>
+        )
+    }
 };
 
 export default Home;
