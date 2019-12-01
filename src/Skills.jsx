cf13c3ebd7bedc19b324ef73b0276fb47f65f08d
Michael Hodges <michaelcshodges@gmail.com>
2019-11-30 16:18:16 -0800

    clean up icon button functionality

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -7,33 +7,36 @@ import { ReactComponent as JSIcon } from './icons/JavaScript-logo.svg'
 
 class Skills extends Component {
     state = {
-        jsDisplayed: false,
+        jsIconDisplayed: false,
         iconDisplayed: false
     }
 
-    jsHandler = (evt) => {
-        //make this module so don't need to repeat for different icons
-        console.log(evt.currentTarget.name)
-
-        //define by evt.name
-        // if (this.state.iconDisplayed) {
-        //     const currentState = Object.entries(this.state)
-        //     const entries = currentState.forEach(
-        //         element => element[1] = false
-        //     )
-        //     const newState = Object.fromEntries(entries)
-        //     this.setState(newState)
-        //     //stop here if current clicked icon is displayed - just turns off current clicked icon (so just add an && condition in if statement)
-        //     this.setState({
-        //         jsDisplayed: true,
-        //         iconDisplayed: true
-        //     })
-        // } else {
-        //     this.setState({
-        //         jsDisplayed: true,
-        //         iconDisplayed: true
-        //     })
-        // }
+    clickHandler = (evt) => {
+        const currentIcon = `${evt.currentTarget.name}Displayed`
+        if (this.state.iconDisplayed && this.state[currentIcon]) {
+            this.setState({
+                [currentIcon]: false,
+                iconDisplayed: false
+            })
+        }
+        else if (this.state.iconDisplayed) {
+            const currentState = Object.entries(this.state)
+            const entries = currentState.forEach(
+                element => element[1] = false
+            )
+            const newState = Object.fromEntries(entries)
+            this.setState(newState)
+            //may need to be in a callback as set state is async
+            this.setState({
+                [currentIcon]: true,
+                iconDisplayed: true
+            })
+        } else {
+            this.setState({
+                [currentIcon]: true,
+                iconDisplayed: true
+            })
+        }
     }
 
 
@@ -49,7 +52,7 @@ class Skills extends Component {
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras orci diam, aliquet porttitor viverra ac, congue et purus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquam augue in sodales rhoncus. Proin vitae leo sit amet mi pulvinar varius in ac turpis. Sed pretium eros rhoncus, tristique augue id, dignissim dolor.
                     </p>
                     <div className="skills--icons">
-                        <button style={{padding: 0}} onClick={this.jsHandler} name='JSIcon'>
+                        <button style={{padding: 0}} onClick={this.clickHandler} name='jsIcon'>
                             <JSIcon className="skills--icon"/>
                         </button>
                     </div>
