7ff36cda85a980f691f977c460ac4ac33ad5e749
Michael Hodges <michaelcshodges@gmail.com>
2019-12-04 12:53:13 +1300

    complete first iteration of project list item component

--- src/Project.jsx
+++ src/Project.jsx
@@ -21,37 +21,72 @@ export default class Project extends Component {
                 {/* on mouse over image, turn grayscale filter down and maybe zoom slowly? */}
                 {
                     (!this.state.isVisible) 
-                    ? <div className='project--info__banner'></div>
-                    : <div className='project--info__banner project--info__banner__large'></div>
+                    ? 
+                    <>
+                        <div className='project--info__banner'></div>
+                        <div className="project--info">
+                            <div className="project--info__container">
+                                <h3 className="project--info__title">{this.props.title}</h3>
+                                <p className="project--info__content">
+                                    <em>
+                                        {this.props.stack}
+                                    </em>
+                                    {/* set up truncation script */}
+                                </p>
+                                {
+                                    this.state.isVisible && 
+                                    <p>
+                                        {this.props.description}
+                                    </p>
+                                }
+                            </div>
+                            <div className="project--info__icons">
+                                <button style={{padding: '0'}} onClick={this.clickHandler}>
+                                    <ExpandMoreIcon style={{fontSize: '35px'}} className='project--info__icon project--info__icon__flipped'/>
+                                </button>
+                                <a href={this.props.github} target="_blank" rel="noopener noreferrer">
+                                    <GitHubIcon style={{fontSize: '32px'}} className='project--info__icon'/>
+                                </a>
+                                <a href={this.props.web} target="_blank" rel="noopener noreferrer">
+                                    <LanguageIcon style={{fontSize: '35px'}} className='project--info__icon'/>
+                                </a>
+                            </div>
+                        </div>
+                    </>
+                    :
+                    <>
+                        <div className='project--info__banner project--info__banner__large'></div>
+                        <div className="project--info project--info__large">
+                            <div className="project--info__container project--info__container__large">
+                                <h3 className="project--info__title">{this.props.title}</h3>
+                                <p className="project--info__content">
+                                    <em>
+                                        {this.props.stack}
+                                    </em>
+                                    {/* set up truncation script */}
+                                </p>
+                                {
+                                    this.state.isVisible && 
+                                    <p className="project--info__content">
+                                        | &nbsp; {this.props.description}
+                                    </p>
+                                }
+                            </div>
+                            <div className="project--info__icons">
+                                <button style={{padding: '0'}} onClick={this.clickHandler}>
+                                    <ExpandMoreIcon style={{fontSize: '35px'}} className='project--info__icon project--info__icon__flipped'/>
+                                </button>
+                                <a href={this.props.github} target="_blank" rel="noopener noreferrer">
+                                    <GitHubIcon style={{fontSize: '32px'}} className='project--info__icon'/>
+                                </a>
+                                <a href={this.props.web} target="_blank" rel="noopener noreferrer">
+                                    <LanguageIcon style={{fontSize: '35px'}} className='project--info__icon'/>
+                                </a>
+                            </div>
+                        </div>
+                    </>
                 }
-                <div className="project--info">
-                    <div className="project--info__container">
-                        <h3 className="project--info__title">{this.props.title}</h3>
-                        <p className="project--info__content">
-                            <em>
-                                {this.props.stack}
-                            </em>
-                            {/* set up truncation script */}
-                        </p>
-                        {
-                            this.state.isVisible && 
-                            <p>
-                                {this.props.description}
-                            </p>
-                        }
-                    </div>
-                    <div className="project--info__icons">
-                        <button style={{padding: '0'}} onClick={this.clickHandler}>
-                            <ExpandMoreIcon style={{fontSize: '35px'}} className='project--info__icon project--info__icon__flipped'/>
-                        </button>
-                        <a href={this.props.github} target="_blank" rel="noopener noreferrer">
-                            <GitHubIcon style={{fontSize: '32px'}} className='project--info__icon'/>
-                        </a>
-                        <a href={this.props.web} target="_blank" rel="noopener noreferrer">
-                            <LanguageIcon style={{fontSize: '35px'}} className='project--info__icon'/>
-                        </a>
-                    </div>
-                </div>
+                
             </div>
         )
     }
--- src/data.js
+++ src/data.js
@@ -11,7 +11,7 @@ export default {
         {
             title: 'Blog',
             stack: 'Handlebars, SASS/SCSS, NodeJs, Express',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam facilisis, est eu pretium sagittis, mi velit fermentum ipsum, vitae placerat erat quam ut lectus.',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam facilisis, est eu pretium sagittis, mi velit fermentum ipsum, vitae placerat erat quam ut lectus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam facilisis, est eu pretium sagittis, mi velit fermentum ipsum, vitae placerat erat quam ut lectus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam facilisis, est eu pretium sagittis, mi velit fermentum ipsum, vitae placerat erat quam ut lectus.',
             github: 'https://github.com/m-hodges/blog',
             web: 'https://m-hodges-blog.herokuapp.com/',
             image: 'https://images.unsplash.com/photo-1525538182201-02cd1909effb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80'
