6c8ee4a6b8caf66893f1bbcfe9bf3d6c1c3a4b3d
Michael Hodges <michaelcshodges@gmail.com>
2019-12-03 17:57:04 +1300

    add portfolio to projects data

--- src/data.js
+++ src/data.js
@@ -10,11 +10,19 @@ export default {
         },
         {
             title: 'Blog',
-            stack: 'NodeJS, Handlebars, Express, SASS/SCSS',
+            stack: 'Handlebars, SASS/SCSS, NodeJs, Express',
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam facilisis, est eu pretium sagittis, mi velit fermentum ipsum, vitae placerat erat quam ut lectus.',
             github: 'https://github.com/m-hodges/blog',
             web: 'https://m-hodges-blog.herokuapp.com/',
             image: 'https://images.unsplash.com/photo-1525538182201-02cd1909effb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80'
+        },
+        {
+            title: 'Portfolio',
+            stack: 'React, SASS/SCSS, Styled Components',
+            description: 'This project itself!',
+            github: 'https://github.com/m-hodges/portfolio',
+            web: '#',
+            image: ''
         }
     ]
 }
