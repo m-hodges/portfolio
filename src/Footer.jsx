ee372e91536c0e1c82e9c267a4a382a3c1c3242d
Michael Hodges <michaelcshodges@gmail.com>
2019-12-03 20:36:42 +1300

    complete rudimentary hover info on footer icons:

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -11,16 +11,27 @@ const Footer = () => {
             <div className='footer--container'>
                 <a className='footer--icon' href="https://github.com/m-hodges" target="_blank" rel="noopener noreferrer">
                     <GitHubIcon style={{fontSize: '31px'}}/>
-                    <p className='footer--text'>Github</p>
+                    <div className='footer--text__container'>
+                        <p className='footer--text'>Github</p>
+                    </div>
                 </a>
-                <a href="https://www.linkedin.com/in/michael-hodges-124089b4" target="_blank" rel="noopener noreferrer">
-                    <LinkedInIcon className='footer--icon' style={{fontSize: '40px'}} />
+                <a className='footer--icon' href="https://www.linkedin.com/in/michael-hodges-124089b4" target="_blank" rel="noopener noreferrer">
+                    <LinkedInIcon style={{fontSize: '40px'}} />
+                    <div className='footer--text__container'>
+                        <p className='footer--text'>LinkedIn</p>
+                    </div>
                 </a>
-                <a href="/other/mhodges_cv.pdf" target="_blank" rel="noopener noreferrer">
-                    <DescriptionIcon className='footer--icon' style={{fontSize: '35px'}} />
+                <a className='footer--icon' href="/other/mhodges_cv.pdf" target="_blank" rel="noopener noreferrer">
+                    <DescriptionIcon style={{fontSize: '35px'}} />
+                    <div className='footer--text__container'>
+                        <p className='footer--text'>CV</p>
+                    </div>
                 </a>
-                <a href="mailto: michaelcshodges@gmail.com" target="_blank" rel="noopener noreferrer">
-                    <EmailIcon className='footer--icon' style={{fontSize: '35px'}} />
+                <a className='footer--icon' href="mailto: michaelcshodges@gmail.com" target="_blank" rel="noopener noreferrer">
+                    <EmailIcon  style={{fontSize: '35px'}} />
+                    <div className='footer--text__container'>
+                        <p className='footer--text'>Contact</p>
+                    </div>
                 </a>
             </div>
         </div>
