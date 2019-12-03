943946c5f512f5c79b2c63d0b5f0fc41ed3a3060
Michael Hodges <michaelcshodges@gmail.com>
2019-12-03 17:49:14 +1300

    more changes to project item component

--- src/Project.jsx
+++ src/Project.jsx
@@ -1,8 +1,20 @@
 import React, { Component } from 'react'
 
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LanguageIcon from '@material-ui/icons/Language';
 
 export default class Project extends Component {
+    state = {
+        isVisible: false
+    }
+
+    clickHandler = () => {
+        this.setState({
+            isVisible: !this.state.isVisible
+        })
+    }
+
     render () {
         return (
             <div className="project" style={{backgroundImage: `url(${this.props.image})`}}>
@@ -16,11 +28,23 @@ export default class Project extends Component {
                             </em>
                             {/* set up truncation script */}
                         </p>
+                        {
+                            this.state.isVisible && 
+                            <p>
+                                {this.props.description}
+                            </p>
+                        }
                     </div>
                     <div className="project--info__icons">
-                        <ExpandMoreIcon fontSize='large' className='project--info__icon'/>
-                        <ExpandMoreIcon fontSize='large' className='project--info__icon'/>
-                        <ExpandMoreIcon fontSize='large' className='project--info__icon'/>
+                        <button style={{padding: '0'}} onClick={this.clickHandler}>
+                            <ExpandMoreIcon style={{fontSize: '35px'}} className='project--info__icon project--info__icon__flipped'/>
+                        </button>
+                        <a href={this.props.github} target="_blank" rel="noopener noreferrer">
+                            <GitHubIcon style={{fontSize: '32px'}} className='project--info__icon'/>
+                        </a>
+                        <a href={this.props.web} target="_blank" rel="noopener noreferrer">
+                            <LanguageIcon style={{fontSize: '35px'}} className='project--info__icon'/>
+                        </a>
                     </div>
                 </div>
             </div>
--- src/data.js
+++ src/data.js
@@ -6,14 +6,15 @@ export default {
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam facilisis, est eu pretium sagittis, mi velit fermentum ipsum, vitae placerat erat quam ut lectus.',
             github: 'https://github.com/m-hodges/chatwell',
             web: 'https://chat-well.herokuapp.com',
-            image: 'https://images.unsplash.com/photo-1461532257246-777de18cd58b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=755&q=80'
+            image: 'https://images.unsplash.com/photo-1512547782667-4786bfbd0754?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80'
         },
         {
             title: 'Blog',
             stack: 'NodeJS, Handlebars, Express, SASS/SCSS',
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam facilisis, est eu pretium sagittis, mi velit fermentum ipsum, vitae placerat erat quam ut lectus.',
             github: 'https://github.com/m-hodges/blog',
-            web: 'https://m-hodges-blog.herokuapp.com/'
+            web: 'https://m-hodges-blog.herokuapp.com/',
+            image: 'https://images.unsplash.com/photo-1525538182201-02cd1909effb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80'
         }
     ]
 }
