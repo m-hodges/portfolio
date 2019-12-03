08084a2915c4f8006d50cb4ac9c3a799a90a94ad
Michael Hodges <michaelcshodges@gmail.com>
2019-12-02 16:08:30 -0800

    add text sizing defaults to projects component

--- src/Project.jsx
+++ src/Project.jsx
@@ -4,18 +4,20 @@ export default class Project extends Component {
     render () {
         return (
             <div className="project">
-                <div className='project--info__banner'>
-
-                </div>
+                <div className='project--info__banner'></div>
                 <div className="project--info">
                     <div className="project--info__container">
-                        <h3 className="project--info__title">A project</h3>
-                        <p className="project--info__content">stuff</p>
+                        <h3 className="project--info__title">ChatWell</h3>
+                        <p className="project--info__content">
+                            <em>
+                                NodeJs, React, Redux, Express, SQLite3, Socket.io
+                            </em>
+                            {/* set up truncation script */}
+                        </p>
                     </div>
                     <div className="project--info__icons">
                     </div>
                 </div>
-
             </div>
         )
     }
