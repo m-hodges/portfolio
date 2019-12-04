81bdea30d623b0a3b921ec9ebd336ee3afa8e391
Michael Hodges <michaelcshodges@gmail.com>
2019-12-04 13:50:03 +1300

    refactor of projects - conditional styles using state, removal of magic strings

--- src/Project.jsx
+++ src/Project.jsx
@@ -4,79 +4,71 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LanguageIcon from '@material-ui/icons/Language';
 
+const info = 'project--info';
+const infoBanner = 'project--info__banner';
+const infoContainer = 'project--info__container';
+const infoContent = 'project--info__content';
+
 export default class Project extends Component {
     state = {
-        isVisible: false
+        isVisible: false,
+        info: info,
+        infoBanner: infoBanner,
+        infoContainer: infoContainer,
+        infoContent: `${infoContent} ${infoContent}__truncated`,
     }
 
     clickHandler = () => {
-        this.setState({
-            isVisible: !this.state.isVisible
-        })
+        if (!this.state.isVisible) {
+            this.setState({
+                isVisible: true,
+                info: `${info} ${info}__large`,
+                infoBanner: `${this.state.infoBanner} ${this.state.infoBanner}__large`,
+                infoContainer: `${this.state.infoContainer} ${this.state.infoContainer}__large`,
+                infoContent: infoContent,
+            })
+        } else {
+            this.setState({
+                isVisible: false,
+                info: info,
+                infoBanner: infoBanner,
+                infoContainer: infoContainer,
+                infoContent: `${infoContent} ${infoContent}__truncated`,
+            })
+        }
     }
 
     render () {
         return (
             <div className="project" style={{backgroundImage: `url(${this.props.image})`}}>
-                {
-                    (!this.state.isVisible) 
-                    ? 
-                    <>
-                        <div className='project--info__banner'></div>
-                        <div className="project--info">
-                            <div className="project--info__container">
-                                <h3 className="project--info__title">{this.props.title}</h3>
-                                    <p className="project--info__content project--info__content__truncated">
-                                        <em>
-                                            {this.props.stack}
-                                        </em>
-                                    </p>
-                            </div>
-                            <div className="project--info__icons">
-                                <button style={{padding: '0'}} onClick={this.clickHandler}>
-                                    <ExpandMoreIcon style={{fontSize: '35px'}} className='project--info__icon project--info__icon__flipped'/>
-                                </button>
-                                <a href={this.props.github} target="_blank" rel="noopener noreferrer">
-                                    <GitHubIcon style={{fontSize: '32px'}} className='project--info__icon'/>
-                                </a>
-                                <a href={this.props.web} target="_blank" rel="noopener noreferrer">
-                                    <LanguageIcon style={{fontSize: '35px'}} className='project--info__icon'/>
-                                </a>
-                            </div>
-                        </div>
-                    </>
-                    :
-                    <>
-                        <div className='project--info__banner project--info__banner__large'></div>
-                        <div className="project--info project--info__large">
-                            <div className="project--info__container project--info__container__large">
-                                <h3 className="project--info__title">{this.props.title}</h3>
+                <div className={this.state.infoBanner}></div>
+                <div className={this.state.info}>
+                    <div className={this.state.infoContainer}>
+                        <h3 className="project--info__title">{this.props.title}</h3>
+                            <p className={this.state.infoContent}>
+                                <em>
+                                    {this.props.stack}
+                                </em>
+                            </p>
+                            {
+                                this.state.isVisible && 
                                 <p className="project--info__content">
-                                    <em>
-                                        {this.props.stack}
-                                    </em>
+                                    | &nbsp; {this.props.description}
                                 </p>
-                                {
-                                    this.state.isVisible && 
-                                    <p className="project--info__content">
-                                        | &nbsp; {this.props.description}
-                                    </p>
-                                }
-                            </div>
-                            <div className="project--info__icons">
-                                <button style={{padding: '0'}} onClick={this.clickHandler}>
-                                    <ExpandMoreIcon style={{fontSize: '35px'}} className='project--info__icon project--info__icon__flipped'/>
-                                </button>
-                                <a href={this.props.github} target="_blank" rel="noopener noreferrer">
-                                    <GitHubIcon style={{fontSize: '32px'}} className='project--info__icon'/>
-                                </a>
-                                <a href={this.props.web} target="_blank" rel="noopener noreferrer">
-                                    <LanguageIcon style={{fontSize: '35px'}} className='project--info__icon'/>
-                                </a>
-                            </div>
-                        </div>
-                    </>
-                }
+                            }
+                    </div>
+                    <div className="project--info__icons">
+                        <button style={{padding: '0'}} onClick={this.clickHandler}>
+                            <ExpandMoreIcon style={{fontSize: '35px'}} className='project--info__icon project--info__icon__flipped'/>
+                        </button>
+                        <a href={this.props.github} target="_blank" rel="noopener noreferrer">
+                            <GitHubIcon style={{fontSize: '32px'}} className='project--info__icon'/>
+                        </a>
+                        <a href={this.props.web} target="_blank" rel="noopener noreferrer">
+                            <LanguageIcon style={{fontSize: '35px'}} className='project--info__icon'/>
+                        </a>
+                    </div>
+                </div>
             </div>
         )
     }
