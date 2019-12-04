daa9d6d437e9934ab62c68ecc6dd2c3ae85b8cea
Michael Hodges <michaelcshodges@gmail.com>
2019-12-04 18:48:48 +1300

    add more flavour text to projects

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export default {
     projects: [
         {
             title: 'ChatWell',
-            stack: 'NodeJS, React, Redux, Express, SQLite3, Socket.io, Styled Components',
+            stack: 'Node.js, React, Redux, Express, Socket.io, SQLite3, Styled Components',
             description: 'This was the final group project for the bootcamp I attended, where I took on a leadership role managing a team of five peers over a week of intense work. I also had the responsibility of all the functionality surrounding websockets here, the core component of the app.  This covered the initial server setup with Socket.io, the configuration of the sockets, an algorithm to decide the most desirable connection candidate, and the front-end user experience engaging with this component. I started this project with zero understanding of websockets and completed it with enough competence to fine tune the chat room connections to the specific needs of the project.',
             github: 'https://github.com/m-hodges/chatwell',
             web: 'https://chat-well.herokuapp.com',
@@ -10,7 +10,7 @@ export default {
         },
         {
             title: 'Blog',
-            stack: 'Handlebars, SASS/SCSS, NodeJs, Express',
+            stack: 'SASS/SCSS, Handlebars, Node.js, Express',
             description: 'A simple blog that was one of the first projects in the bootcamp I attended. This was initially hardcoded in pure HTML and CSS, but I later refactored it with templates to modularize it, Handlebars being the package of choice. That being said, my personal goal for this project was to gain competence in SASS/SCSS, so the focus here was on creating a visually attractive and responsive blog while utilizing the benefits of a preprocessing language rather than expanding functionality to full CRUD routing, which is a stretch goal yet to be addressed.',
             github: 'https://github.com/m-hodges/blog',
             web: 'https://m-hodges-blog.herokuapp.com/',
@@ -19,10 +19,10 @@ export default {
         {
             title: 'Portfolio',
             stack: 'React, SASS/SCSS, Styled Components',
-            description: 'This project itself!',
+            description: 'This project itself, I have aimed for a simple and elegant theme and a clean, crisp user experience. The functionality of this project itself is very minimal, focusing purely on front-end, so I intend to use this as a display of my front-end skills, with future improvements planned to include entirely new themes to further show these off.',
             github: 'https://github.com/m-hodges/portfolio',
-            web: '#',
-            image: ''
+            web: '/',
+            image: 'https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=752&q=80'
         }
     ]
 }
