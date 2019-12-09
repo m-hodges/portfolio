5c14b81d327f585e35b7ea70525400bb9f390cb7
m-hodges <50115036+m-hodges@users.noreply.github.com>
2019-12-09 17:14:59 +1300

    begin experiments with refs

--- src/About.jsx
+++ src/About.jsx
@@ -4,11 +4,17 @@ import {
     PageFlexContainer
 } from './styles/styled_components/Containers'
 
+const scrollToRef = (ref) => window.scrollTo(0, ref.current.offsetTop)   
+
 const About = () => {
+    const titleRef = React.createRef()
+    const executeScroll = () => scrollToRef(titleRef)
+
+
     return (
         <PageFlexContainer left id='aboutPage'>
             <div className="about">
-                <h2 className="about--title">
+                <h2 className="about--title" ref={titleRef}>
                     About
                     <hr />
                 </h2>
@@ -16,6 +22,7 @@ const About = () => {
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras orci diam, aliquet porttitor viverra ac, congue et purus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquam augue in sodales rhoncus. Proin vitae leo sit amet mi pulvinar varius in ac turpis. Sed pretium eros rhoncus, tristique augue id, dignissim dolor.
                 </p>
             </div>
+            <button onClick={executeScroll}>click</button>
         </PageFlexContainer>
     );
 };
