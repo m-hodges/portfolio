0d1c6ba541ec33365b032b5bbeab50de29c638b8
m-hodges <michaelcshodges@gmail.com>
2020-01-01 19:26:40 +1300

    add animation css and test

--- src/About.jsx
+++ src/About.jsx
@@ -11,9 +11,9 @@ const About = React.forwardRef((props, ref) => {
                 <div className="about">
                     <h2 className="about--title">
                         About
-                        <hr />
+                        <hr className='scroll-in scroll-in--visible'/>
                     </h2>
-                    <p> 
+                    <p className='fade-in fade-in--visible'> 
                         I am a full-stack developer with a special interest in decentralization, particularly to do with large-scale content distribution networks and the problems that come with the costly storage solutions these platforms rely upon. I am currently building upon my skills on both the front and back-end so that I can further explore this area in the hopes of contributing to or creating new open source solutions to the issue that also, importantly, provides a clean and easy to operate user experience to provide maximum accessibility to the average user.
                     </p>
                 </div>
