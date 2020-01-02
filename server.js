736d2519c200f2e6a6d39365183994ab52604a9e
m-hodges <michaelcshodges@gmail.com>
2020-01-02 21:28:22 +1300

    cores adj

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 const express = require('express')
 const path = require('path')
 
-const router = require('./routes')
 
 const server = express()
 
 server.use(express.static(__dirname))
 server.use(express.static(path.join(__dirname, 'build')))
-server.use((req, res) => {
-    if (!req.secure) {
-        res.redirect('https://' + req.headers.host)
-    }
-})
+// server.use((req, res) => {
+//     if (!req.secure) {
+//         res.redirect('https://' + req.headers.host)
+//     }
+// })
+server.use('*', function(req, res, next) {
+    //replace localhost:8080 to the ip address:port of your server
+    res.header("Access-Control-Allow-Origin", "https://m-hodges.com");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Credentials', true);
+    next(); 
+});
+
+const router = require('./routes')
 server.use('/', router)
 
 
