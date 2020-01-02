89b8b8e3466a461d2325882c77d1f5096ca6cc9f
m-hodges <michaelcshodges@gmail.com>
2020-01-02 21:53:46 +1300

    cors adj

--- server.js
+++ server.js
@@ -4,24 +4,29 @@ const cors = require('cors')
 
 const server = express()
 
+server.options('*', cors())
+
+const corsOptions = {
+    origin: 'https://m-hodges.com'
+}
+
 server.use(express.static(__dirname))
 server.use(express.static(path.join(__dirname, 'build')))
 server.use('*', (req, res, next) => {
-    //replace localhost:8080 to the ip address:port of your server
-    res.header("Access-Control-Allow-Origin", "https://m-hodges.com");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    res.header('Access-Control-Allow-Credentials', true);
-    next(); 
-});
-server.options('*', cors())
+    res.header("Access-Control-Allow-Origin", "https://m-hodges.com")
+    res.header("Access-Control-Allow-Headers", "X-Requested-With")
+    res.header('Access-Control-Allow-Headers', 'Content-Type')
+    res.header('Access-Control-Allow-Credentials', true)
+    next()
+})
+server.use(cors(corsOptions))
 server.use((req, res) => {
     if (!req.secure) {
         res.redirect('https://' + req.headers.host)
     }
 })
+
 const router = require('./routes')
 server.use('/', router)
-
 
 module.exports = server
