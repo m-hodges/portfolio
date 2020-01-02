1a0d58419ea774c2888118f4c0d57786750cce69
m-hodges <michaelcshodges@gmail.com>
2020-01-02 20:29:56 +1300

    add https redirect to routes

--- routes.js
+++ routes.js
@@ -4,8 +4,17 @@ const path = require('path')
 const router = express.Router()
 
 router.get('/*', (req, res) => {
-    console.log('Hit the route')
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+    // console.log('Hit the route')
+    // var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+    // console.log(req.protocol)
+    // console.log(req.get('host'))
+    // console.log(req.originalUrl)
+    if (req.protocol === 'http') {
+        // console.log('https' + '://' + req.get('host'))
+        res.redirect('https' + '://' + req.get('host'))
+    } else {
+        res.sendFile(path.join(__dirname, 'build', 'index.html'))
+    }
 })
 
 module.exports = router
