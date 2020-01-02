a7003fc6ca7d9cbdfe0980e0c5686b81716ef635
m-hodges <michaelcshodges@gmail.com>
2020-01-02 20:46:42 +1300

    adjustments to https forwarding

--- routes.js
+++ routes.js
@@ -3,18 +3,8 @@ const path = require('path')
 
 const router = express.Router()
 
-router.get('/*', (req, res) => {
-    // console.log('Hit the route')
-    // var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-    // console.log(req.protocol)
-    // console.log(req.get('host'))
-    // console.log(req.originalUrl)
-    if (req.protocol === 'http') {
-        // console.log('https' + '://' + req.get('host'))
-        res.redirect('https' + '://' + req.get('host'))
-    } else {
-        res.sendFile(path.join(__dirname, 'build', 'index.html'))
-    }
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const server = express()
 
 server.use(express.static(__dirname))
 server.use(express.static(path.join(__dirname, 'build')))
+server.use((req, res) => {
+    if (!req.secure) {
+        res.redirect('https://' + req.headers.host + req.url)
+    }
+})
 server.use('/', router)
+
 
 module.exports = server
