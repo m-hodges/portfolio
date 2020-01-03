8e3ef6c7018bc0c18dce6735e2f5f95c314b6eb8
m-hodges <michaelcshodges@gmail.com>
2020-01-03 21:20:56 +1300

    prune https server code and cors

--- routes.js
+++ routes.js
@@ -1,19 +1,11 @@
 const express = require('express')
 const path = require('path')
-// const cors = require('cors')
 
 const router = express.Router()
 
-// const corsPermissions = {
-//     origin: 'https://m-hodges.com'
-// }
-
 router.get('*', (req, res, next) => {
-    // if (!req.secure) {
-    //     res.redirect('https://' + req.headers.host)
-    // } else {
-        res.sendFile(path.join(__dirname, 'build', 'index.html'))
-    // }
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+
 })
 
 module.exports = router
