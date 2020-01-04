b342893c3d8630171f24c118d325951a4b1bb447
m-hodges <michaelcshodges@gmail.com>
2020-01-04 22:03:30 +1300

    add state count random interval timer

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
@@ -8,6 +8,17 @@ const scrollToRef = (ref) => window.scrollTo(0, ref.current.offsetTop)
 
 const Home = React.forwardRef((props, ref) => {
     const executeScroll = () => scrollToRef(ref)
+    let [count, setCount] = useState(0)
+    useEffect(() => {
+        const randomCount = (max) => (
+            Math.floor(Math.random() * Math.floor(max))
+        )
+        // console.log(randomCount(3))
+        const intervalTimer = setInterval(() => setCount(randomCount(3)), 3000)
+        return () => clearInterval(intervalTimer)
+    })
+
+
     return (
         <PageFlexContainer>
             <div className='spacer'></div>
